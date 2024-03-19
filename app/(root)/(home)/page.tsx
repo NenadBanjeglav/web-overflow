f0b6b36246668dcc0e0f7d3f43c2b924f05cfa7f
NenadBanjeglav <nenadmobmail@gmail.com>
2024-03-19 14:42:56 +0100

    implement ThemeProvider

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,8 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="h-screen">
-      <UserButton />
-      <h1>HOME</h1>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
