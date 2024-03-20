61b2e1c67ba22b6bd45e1a4831ae006361b6fc0e
NenadBanjeglav <nenadmobmail@gmail.com>
2024-03-20 12:30:23 +0100

    implement Navbar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div className="h-screen">
-      <UserButton afterSignOutUrl="/" />
+      <h1>HOME</h1>
     </div>
   );
 }
